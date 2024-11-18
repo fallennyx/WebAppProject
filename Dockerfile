e06addcf171db8c59d28c027e208b342404e0341
manny <1eakanmu@gmail.com>
2024-11-18 02:36:36 -0500

    lo3

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,17 @@ FROM python:3
 
 ENV HOME /root
 WORKDIR /root
+
 COPY . .
+
+RUN apt-get update && \
+    apt-get install -y libmagic1 && \
+    rm -rf /var/lib/apt/lists/*
+
 RUN pip3 install -r requirements.txt
+
 EXPOSE 8080
+
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
 RUN chmod +x /wait
 
--- server.py
+++ server.py
@@ -13,21 +13,26 @@ import html
 import socketserver
 from util.multipart import parse_multipart
 import os
+import magic
 
-
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse312"]
+chat_collection =db["chat"]
+users = db["users"]
+mime=magic.Magic(mime=True)
+os.makedirs('public/media-uploads', exist_ok=True)
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
+
     # Initializes Router object, adds routes to Router object,
-    # loads files into dictionary, calls loads files to load files into dictionary
     def __init__(self, request, client_address, server):
         self.router = Router()
-
-        self.mongo_client = MongoClient("mongo")
-        self.db = self.mongo_client["cse312"]
-        self.chat_collection = self.db["chat"]
-        self.users = self.db["users"]
+        os.makedirs('public/media-uploads', exist_ok=True)
 
 
+
+
+        # Define routes
         self.router.add_route("GET", "/", self.index_path, True)
         self.router.add_route("GET", "/public/style.css", self.css_path, True)
         self.router.add_route("GET", "/public/webrtc.js", self.js_path, True)
@@ -40,8 +45,8 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.router.add_route("POST", "/register", self.register, True)
         self.router.add_route("POST", "/login", self.login, True)
         self.router.add_route("POST", "/logout", self.logout, True)
-        self.router.add_route("POST", "/media-uploads", self.upload_image, False)
-        self.router.add_route("GET", "/public/media-uploads", self.media_uploads_path, False)
+        self.router.add_route("POST", "/media-uploads", self.upload_media, False)
+        self.router.add_route("GET", "/public/media-uploads/", self.media_uploads_path, False)
 
         self.files = {
             "public/index.html": "text/html",
@@ -59,7 +64,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         }
         self.loaded_files = {}
         self.load_files()
-        self.visits=1
+        self.visits = 1
         super().__init__(request, client_address, server)
 
     def handle(self):
@@ -117,7 +122,6 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             except Exception as e:
                 print(f"Error loading {filename}: {e}")
 
-
     # Handles index path
     def index_path(self, request, handler):
         if "public/index.html" in self.loaded_files:
@@ -128,24 +132,25 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             return
 
         if "visits" in request.cookies:
-            self.visits = int(request.cookies["visits"])+1
+            self.visits = int(request.cookies["visits"]) + 1
             content = content.replace(b'{{visits}}', str(self.visits).encode())
         else:
             content = content.replace(b'{{visits}}', str(self.visits).encode())
 
         xsrf_token = None
         if "auth_token" in request.cookies:
-            user = self.users.find_one({"auth_token": request.cookies["auth_token"]}, {"username": 1, "_id": 0})
+            user = users.find_one({"auth_token": request.cookies["auth_token"]}, {"username": 1, "_id": 0})
             if user:
-                xsrf_token = user.get("xsrf_token",None)
+                xsrf_token = user.get("xsrf_token", None)
                 if xsrf_token is None:
                     print("generating new token")
                     xsrf_token = str(uuid.uuid4())
-                    self.users.update_one({"auth_token": request.cookies["auth_token"]}, {"$set": {"xsrf_token": xsrf_token}})
+                    users.update_one({"auth_token": request.cookies["auth_token"]}, {"$set": {"xsrf_token": xsrf_token}})
                     print("token generated and added to db")
-                content = (content.replace(
+                content = content.replace(
                     b'{{auth_section}}',
-                    b'<form action="/logout" method="post"><input type="submit" value="Logout"></form>'))
+                    b'<form action="/logout" method="post"><input type="submit" value="Logout"></form>'
+                )
             else:
                 content = content.replace(
                     b'{{auth_section}}',
@@ -192,10 +197,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             content = content.replace(b'{{xsrf_token}}', xsrf_token.encode())
         else:
             content = content.replace(b'{{xsrf_token}}', b'')
-        self.send_response(content, "text/html", 200,visits=self.visits)
-
-
-
+        self.send_response(content, "text/html", 200, visits=self.visits)
 
     # Handles CSS path
     def css_path(self, request, handler):
@@ -222,7 +224,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     # Handles image path
     def image_path(self, request, handler):
-        image = request.path[1:]
+        image = request.path.lstrip('/')
         if image in self.loaded_files:
             content = self.loaded_files[image]["content"]
             content_type = self.loaded_files[image]["content_type"]
@@ -241,7 +243,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def send_response(self, content, content_type, status_code=200, visits=None, auth_token=None):
         header = (f"HTTP/1.1 {status_code} OK\r\n"
-                  f"Content-Type: {content_type};charset=utf-8\r\n"
+                  f"Content-Type: {content_type}; charset=utf-8\r\n"
                   f"Content-Length: {len(content)}\r\n"
                   "X-Content-Type-Options: nosniff\r\n")
         if auth_token:
@@ -268,7 +270,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             password = data[1].encode('utf-8')
             hashedPass = bcrypt.hashpw(password, bcrypt.gensalt())
             print("hashed password good")
-            self.db.users.insert_one({"username": username, "password": hashedPass})
+            db.users.insert_one({"username": username, "password": hashedPass})
             self.send_302_found()
         except Exception as e:
             print(f"Error in register, from register function: {e}")
@@ -280,7 +282,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             data = extract_credentials(request)
             username = data[0]
             password = data[1].encode('utf-8')
-            user = self.db.users.find_one({"username": username})
+            user = db.users.find_one({"username": username})
             if user:
                 hashed = user["password"]
                 if bcrypt.checkpw(password, hashed):
@@ -288,9 +290,9 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     hash_object = hashlib.sha256()
                     hash_object.update(token)
                     hashedToken = hash_object.hexdigest()
-                    self.db.users.update_one({"username": username}, {"$set": {"auth_token": hashedToken}}, upsert=True)
-                    self.visits=0
-                    self.send_302_found(visits=self.visits,auth_token=hashedToken)
+                    db.users.update_one({"username": username}, {"$set": {"auth_token": hashedToken}}, upsert=True)
+                    self.visits = 0
+                    self.send_302_found(visits=self.visits, auth_token=hashedToken)
                 else:
                     print("password not good")
                     self.send_302_found()
@@ -309,14 +311,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             message = data['message']
             if request.cookies.get("auth_token") is not None and request.cookies.get("auth_token") != "expired":
                 auth = request.cookies.get("auth_token")
-                username = self.users.find_one({"auth_token":  auth}, {"username": 1, "_id": 0})
-                xsrf= self.users.find_one({"auth_token": auth}, {"xsrf_token": 1, "_id": 0})
+                username = users.find_one({"auth_token": auth}, {"username": 1, "_id": 0})
+                xsrf = users.find_one({"auth_token": auth}, {"xsrf_token": 1, "_id": 0})
                 if username:
                     if data["xsrf_token"] != xsrf["xsrf_token"]:
                         print("XSRF token not valid")
                         self.send403error()
                         return
-                    result = self.chat_collection.insert_one({"username": username["username"], "message": message})
+                    result = chat_collection.insert_one({"username": username["username"], "message": message})
                     response = json.dumps({"message_id": str(result.inserted_id)})
                     self.send_response(response.encode('utf-8'), 'application/json')
                     return
@@ -324,7 +326,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     print("User not found")
             else:
                 print("No auth token")
-            result = self.chat_collection.insert_one({"username": user, "message": message})
+            result = chat_collection.insert_one({"username": user, "message": message})
             response = json.dumps({"message_id": str(result.inserted_id)})
             self.send_response(response.encode('utf-8'), 'application/json')
 
@@ -335,42 +337,48 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     # Handles retrieving chat messages
     def get_chat_messages(self, request, handler):
         try:
-            messages = list(self.chat_collection.find())
+            messages = list(chat_collection.find())
             formatted_messages = []
             for msg in messages:
                 message_content = msg['message']
-                if message_content.strip().startswith('<img'):
+                if message_content.strip().startswith('<img') or message_content.strip().startswith('<video'):
                     formatted_message = message_content  # Render as-is
                 else:
-                    formatted_message = html.escape(message_content)  # Escape for security
+                    formatted_message = html.escape(message_content)
+                    print("escaped")
                 formatted_messages.append({
-                "username": msg['username'],
-                "message": formatted_message,
-                "id": str(msg['_id'])
-            })
+                    "username": msg['username'],
+                    "message": formatted_message,
+                    "id": str(msg['_id'])
+                })
             response = json.dumps(formatted_messages)
             self.send_response(response.encode('utf-8'), 'application/json')
         except Exception as e:
             print(f"Error in get_chat_messages: {e}")
             self.send404error()
 
-
-    def delete_chat_message(self, request,handler):
+    def delete_chat_message(self, request, handler):
         try:
+
             message_id = request.path.split('/')[-1]
             auth_token = request.cookies.get("auth_token")
             if not auth_token:
-                print("No auth token")
-                self.send_302_found()
-                return
+                if chat_collection.find_one({"_id": ObjectId(message_id)})["username"]=="Guest" :
+                    result = chat_collection.delete_one({"_id": ObjectId(message_id)})
+                    self.send_response(b'', 'application/json', 204)
+                    return
+                else:
+                    print("No auth token")
+                    self.send_302_found()
+                    return
 
-            user = self.users.find_one({"auth_token": auth_token}, {"username": 1, "_id": 0})
+            user = users.find_one({"auth_token": auth_token}, {"username": 1, "_id": 0})
             if not user:
                 print("User not found")
                 self.send_302_found(visits=request.cookies.get("visits"))
                 return
 
-            message = self.chat_collection.find_one({"_id": ObjectId(message_id)})
+            message = chat_collection.find_one({"_id": ObjectId(message_id)})
             if not message:
                 print("Message not found")
                 self.send403error()
@@ -380,7 +388,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                 self.send403error()
                 return
 
-            result = self.chat_collection.delete_one({"_id": ObjectId(message_id)})
+            result = chat_collection.delete_one({"_id": ObjectId(message_id)})
             self.send_response(b'', 'application/json', 204)
         except Exception as e:
             print(f"Error in delete_chat_message: {e}")
@@ -390,7 +398,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     def logout(self, request, handler):
         try:
             if "auth_token" in request.cookies:
-                self.db.users.update_one(
+                db.users.update_one(
                     {"auth_token": request.cookies.get("auth_token")},{"$unset": {"auth_token": ""}})
                 self.send_302_found(auth_token="expired")
             else:
@@ -424,95 +432,95 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.request.sendall(header.encode() + b"403 Forbidden")
 
     # Sends 302 Found redirect
-    def send_302_found(self, visits=None,auth_token=None):
-        content = b''
+    def send_302_found(self, visits=None, auth_token=None,content=None):
+        if content:
+            content_type=mime.from_buffer(content)
+        else:
+            content_type="text/html"
+            content=b" "
+
         header = (f"HTTP/1.1 302 Found\r\n"
                   "Location: /\r\n"
-                  "Content-Type: text/html; charset=utf-8\r\n"
+                  f"Content-Type:{content_type}; charset=utf-8\r\n"
                   f"Content-Length: {len(content)}\r\n"
                   "X-Content-Type-Options: nosniff\r\n")
         if visits:
             header += (f"Set-Cookie: visits={visits}; Max-Age=3600; Path=/\r\n")
-        if auth_token == "expired":
-            header += "Set-Cookie: auth_token=; HttpOnly; Max-Age=0; Path=/\r\n"
+            if auth_token == "expired":
+                header += "Set-Cookie: auth_token=; HttpOnly; Max-Age=0; Path=/\r\n"
         if auth_token:
-            header += f"Set-Cookie:auth_token={auth_token}; HttpOnly; Max-Age=3600; Path=/\r\n"
+                header += f"Set-Cookie: auth_token={auth_token}; HttpOnly; Max-Age=3600; Path=/\r\n"
         header += "\r\n"
         self.request.sendall(header.encode() + content)
 
-    def upload_image(self, request, handler):
+    def upload_media(self, request, handler):
         try:
             # Step 1: Parse the multipart body
             multipart = parse_multipart(request)
             if multipart is None:
-                print("multipart failed")
+                print("Failed to parse multipart data")
+                return self.send_302_found()
 
-
-            # Step 2: Validate and save the file
-            image_content=None
             for part in multipart.parts:
-                if part.name.lower()=="upload":
-                    image_content=part.content
-                else:
-                    print("no image content found")
+                if part.name.lower() == "upload":
+                    file_content = part.content
+                    break
+            file_type=mime.from_buffer(file_content)
+            print(file_type)
+            unique_id = uuid.uuid4().hex
+            ex=file_type.split("/")[1]
+            filename = f"file_{unique_id}.{ex}"
 
-            filename = f"image_{uuid.uuid4()}.jpg"  # Use a secure naming convention
             upload_dir = "public/media-uploads"
-            os.makedirs(upload_dir, exist_ok=True)
-            print("makedir successful")
             file_path = os.path.join(upload_dir, filename)
 
             with open(file_path, "wb") as f:
-                f.write(image_content)
-            path = "public/media-uploads"
-            if os.path.exists(path):
-                print(f"The path '{path}' exists.")
-            else:
-                print(f"{path}does not exist")
-
-            path = f"public/media-uploads/{filename}"
-            if os.path.exists(path):
-                print(f"The path '{path}' exists.")
-            else:
-                print(f"{path}does not exist")
-
-
-            print(f"Image saved to {file_path}")
-
-            # Step 3: Add the uploaded image to the chat database
+                f.write(file_content)
+            print(f"File saved to {file_path}")
             username = "Guest"
             if request.cookies.get("auth_token"):
-                user = self.users.find_one(
-                    {"auth_token": request.cookies["auth_token"]}, {"username": 1, "_id": 0}
+                user = users.find_one(
+                    {"auth_token": request.cookies.get("auth_token")}, {"username": 1, "_id": 0}
                 )
                 if user:
                     username = user["username"]
 
-            message = f'<img src="/{file_path}" alt="Uploaded Image"/>'
-            self.chat_collection.insert_one({"username": username, "message": message})
+            relative_path = f"/public/media-uploads/{filename}"
+            if ex in ['jpg', 'png', 'gif',"jpeg"]:
+                message = f'<img src="{relative_path}" alt="Uploaded Image"/>'
+            elif ex == 'mp4':
+                message = f'''
+                <video controls autoplay muted>
+                 <source src={relative_path} type="video/mp4">
+                 Your browser does not support video playback
+                </video>
+                '''
 
-            # Step 4: Redirect to the homepage
-            self.send_302_found()
+            chat_collection.insert_one({"username": username, "message": message})
+            self.send_302_found(content=file_content)
 
         except Exception as e:
-            print(f"Error during image upload: {e}")
+            print(f"Error during media upload: {e}")
             self.send_302_found()
 
-    def media_uploads_path(self, request, handler):
 
-        file_path=request.path[1:]
-        # Remove leading '/'
+    def media_uploads_path(self, request, handler):
+        file_path = request.path.lstrip('/')
+        safe_path = os.path.normpath(file_path)
+        if not safe_path.startswith("public/media-uploads/"):
+            print("Attempted directory traversal attack")
+            return self.send404error()
         try:
             with open(file_path, "rb") as f:
                 content = f.read()
-                content_type = "image/jpeg"
-            self.send_response(content, content_type)
+                file_type = mime.from_buffer(content)
+            self.send_response(content, file_type)
         except FileNotFoundError:
-            print("file not found")
+            print("File not found:", file_path)
             self.send404error()
-
-
-
+        except Exception as e:
+            print(f"Error serving media file {file_path}: {e}")
+            self.send404error()
 
 
 def main():
@@ -521,7 +529,6 @@ def main():
     socketserver.TCPServer.allow_reuse_address = True
 
     server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler)
-#    server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler) for HW 4
     print("Listening on port " + str(port))
     server.serve_forever()
 
